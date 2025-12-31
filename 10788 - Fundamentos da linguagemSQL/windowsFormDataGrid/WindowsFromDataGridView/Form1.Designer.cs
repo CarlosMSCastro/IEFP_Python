81c625552a3dfef7a531b0979db612437cc9256f
Carlos <carloscastro96@live.com.pt>
2025-12-31 03:31:45 +0000

    WindowsForm_DataGridView

---
+++ 10788 - Fundamentos da linguagemSQL/windowsFormDataGrid/WindowsFromDataGridView/Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace WindowsFromDataGridView
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.Label label1;
+            this.cmbTipo = new System.Windows.Forms.ComboBox();
+            this.dgvDados = new System.Windows.Forms.DataGridView();
+            label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDados)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // cmbTipo
+            // 
+            this.cmbTipo.FormattingEnabled = true;
+            this.cmbTipo.Location = new System.Drawing.Point(12, 25);
+            this.cmbTipo.Name = "cmbTipo";
+            this.cmbTipo.Size = new System.Drawing.Size(141, 21);
+            this.cmbTipo.TabIndex = 0;
+            // 
+            // dgvDados
+            // 
+            this.dgvDados.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvDados.Location = new System.Drawing.Point(15, 74);
+            this.dgvDados.Name = "dgvDados";
+            this.dgvDados.Size = new System.Drawing.Size(561, 215);
+            this.dgvDados.TabIndex = 1;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new System.Drawing.Point(12, 9);
+            label1.Name = "label1";
+            label1.Size = new System.Drawing.Size(89, 13);
+            label1.TabIndex = 2;
+            label1.Text = "Tipo de Utilizador";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(596, 296);
+            this.Controls.Add(label1);
+            this.Controls.Add(this.dgvDados);
+            this.Controls.Add(this.cmbTipo);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDados)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cmbTipo;
+        private System.Windows.Forms.DataGridView dgvDados;
+    }
+}
+
---
+++ 10788 - Fundamentos da linguagemSQL/windowsFormDataGrid/WindowsFromDataGridView/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFromDataGridView
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            cmbTipo.Items.Add("Clientes");
+            cmbTipo.Items.Add("Fornecedores");
+            cmbTipo.Items.Add("Funcionários");
+
+            cmbTipo.SelectedIndexChanged += CmbTipo_SelectedIndexChanged;
+        }
+
+        private void CmbTipo_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string connectionString = "Data Source=localhost\\SQLEXPRESS;Initial Catalog=PRIDEMO;Integrated Security=True;";
+            
+            if (cmbTipo.SelectedItem == null)
+                return;
+
+            string opcaoSelecionada = cmbTipo.SelectedItem.ToString();
+
+            string query = "";
+
+            if (opcaoSelecionada == "Clientes")
+            {
+                query = "SELECT Cliente, Nome, NumContrib, Pais, TotalDeb FROM Clientes";
+            }
+            else if (opcaoSelecionada == "Fornecedores")
+            {
+                query = "SELECT Fornecedor, Nome, NumContrib, Pais, TotalDeb FROM Fornecedores";
+            }
+            else if (opcaoSelecionada == "Funcionários")
+            {
+                query = "SELECT Codigo, Nome, Telefone, DataNascimento FROM Funcionarios";
+            }
+
+            DataTable dt = new DataTable();
+
+ 
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                SqlDataAdapter adapter = new SqlDataAdapter(query, conn);
+
+                adapter.Fill(dt);
+
+                dgvDados.DataSource = dt;
+            }
+
+        }
+    }
+}
