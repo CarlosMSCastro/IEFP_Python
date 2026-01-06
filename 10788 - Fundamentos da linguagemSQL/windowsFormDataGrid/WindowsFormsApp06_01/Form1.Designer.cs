84fb3f50d2d11cf21198708d322186c4f4879579
Carlos <carloscastro96@live.com.pt>
2026-01-06 11:55:51 +0000

    horario_atualizado_aula06_01

---
+++ 10788 - Fundamentos da linguagemSQL/windowsFormDataGrid/WindowsFormsApp06_01/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace WindowsFormsApp06_01
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+        private void InitializeComponent()
+        {
+            this.cmbClientes = new System.Windows.Forms.ComboBox();
+            this.lstFornecedores = new System.Windows.Forms.ListBox();
+            this.txtResultado = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // cmbClientes
+            // 
+            this.cmbClientes.Location = new System.Drawing.Point(221, 120);
+            this.cmbClientes.Name = "cmbClientes";
+            this.cmbClientes.Size = new System.Drawing.Size(121, 21);
+            this.cmbClientes.TabIndex = 0;
+            this.cmbClientes.SelectedIndexChanged += new System.EventHandler(this.cmbClientes_SelectedIndexChanged_1);
+            // 
+            // lstFornecedores
+            // 
+            this.lstFornecedores.Location = new System.Drawing.Point(459, 153);
+            this.lstFornecedores.Name = "lstFornecedores";
+            this.lstFornecedores.Size = new System.Drawing.Size(120, 95);
+            this.lstFornecedores.TabIndex = 1;
+            this.lstFornecedores.SelectedIndexChanged += new System.EventHandler(this.lstFornecedores_SelectedIndexChanged_1);
+            // 
+            // txtResultado
+            // 
+            this.txtResultado.Location = new System.Drawing.Point(253, 228);
+            this.txtResultado.Name = "txtResultado";
+            this.txtResultado.Size = new System.Drawing.Size(100, 20);
+            this.txtResultado.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(818, 392);
+            this.Controls.Add(this.cmbClientes);
+            this.Controls.Add(this.lstFornecedores);
+            this.Controls.Add(this.txtResultado);
+            this.Name = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+        #endregion
+
+
+        private System.Windows.Forms.ComboBox cmbClientes;
+        private System.Windows.Forms.ListBox lstFornecedores;
+        private System.Windows.Forms.TextBox txtResultado;
+    }
+}
+
---
+++ 10790 - Projeto de Programação/exercicos_aulas/WindowsFormsApp06_01/Form1.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp06_01
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string cnn = "Data Source=localhost\\SQLEXPRESS;Initial Catalog=PRIDEMO;Integrated Security=True;";
+            using (var conn = new SqlConnection(cnn))
+            {
+                conn.Open();
+
+                var cmdClientes = new SqlCommand("SELECT cliente, nome FROM clientes", conn);
+                var readerClientes = cmdClientes.ExecuteReader();
+                List<Cliente> clientes = new List<Cliente>();
+                while (readerClientes.Read())
+                {
+                    var cliente = new Cliente
+                    {
+                        codigo = readerClientes["cliente"].ToString(),
+                        nome = readerClientes["nome"].ToString(),
+                    };
+                    clientes.Add(cliente);
+                }
+                readerClientes.Close();
+                cmbClientes.DataSource = clientes;
+                cmbClientes.ValueMember = "codigo";
+                cmbClientes.DisplayMember = "nome";
+
+                var cmdFornecedores = new SqlCommand("SELECT fornecedor, nome FROM fornecedores", conn);
+                var readerFornecedores = cmdFornecedores.ExecuteReader();
+                while (readerFornecedores.Read())
+                {
+                    lstFornecedores.Items.Add(new Fornecedor
+                    {
+                        codigo = readerFornecedores["fornecedor"].ToString(),
+                        nome = readerFornecedores["nome"].ToString()
+                    });
+                }
+            }
+        }
+        class Cliente
+        {
+            public string codigo {  get; set; }
+            public string nome {  get; set; }
+        }
+        class Fornecedor
+        {
+            public string codigo { get; set; }
+            public string nome { get; set; }
+            public override string ToString()
+            {
+                return nome;
+            }
+        }
+
+        private void cmbClientes_SelectedIndexChanged_1(object sender, EventArgs e)
+        {
+            if (cmbClientes.SelectedIndex != -1)
+            {
+                txtResultado.Text = cmbClientes.Text.ToString();
+            }
+        }
+
+        private void lstFornecedores_SelectedIndexChanged_1(object sender, EventArgs e)
+        {
+            Fornecedor escolhido = lstFornecedores.SelectedItem as Fornecedor;
+            txtResultado.Text = escolhido.codigo + " - " + escolhido.nome;
+        }
+    }
+}
